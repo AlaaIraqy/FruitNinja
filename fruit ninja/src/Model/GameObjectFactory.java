12c280952980f34e4aaf218b86f187b889f4d894
omarkhaled99 <48597734+omarkhaled99@users.noreply.github.com>
2019-05-12 22:59:24 +0200

    place of mouse2

---
+++ fruit ninja/src/Model/GameObjectFactory.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.util.Random;
+
+public class GameObjectFactory {
+      public GameObject createFruit() {
+    	  int i;
+    	  Random rand  = new Random();
+    	  i = rand.nextInt(7);
+    	  if(i==0) {
+    		  return new Apple();}
+    	  else if(i==1) {
+    		   return new Banana();
+    	   }
+    	  else if(i==2) {
+    		   return new Basaha();
+    	   } 
+    	  else if(i==3) {
+    		   return new Peach();
+    	   } 
+    	  else if(i==4) {
+    		   return new Sandia();
+    	   } 
+    	  else if(i==5) {
+    		   return new Boom();
+    	   } 
+    	  else {
+    		  return null;
+    	  }
+    	  }
+      }
+
